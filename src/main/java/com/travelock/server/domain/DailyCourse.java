8c2c4503e190d27a97ca26952ce76dcadf4309d8
hyewon <jhjsjym@gmail.com>
2024-09-22 09:24:48 +0900

    메인서비스 관련 domain 추가_주혜원

--- src/main/java/com/travelock/server/domain/DailyCourse.java
+++ src/main/java/com/travelock/server/domain/DailyCourse.java
@@ -17,6 +17,23 @@ public class DailyCourse implements Serializable {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long dailyCourseId;
 
+    @Column(columnDefinition = "INT COMMENT '일자 정보(N일차)'")
+    private Integer dayNum;
+
+    // Daily Course : Member = N : 1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // Daily Course : Full Course = N : 1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "full_course_id", nullable = false)
+    private FullCourse fullCourse;
+
+    // Daily Course : Daily Block Connect = 1 : N
+    @OneToMany(mappedBy = "dailyCourse")
+    private List<DailyBlockConnect> dailyBlockConnects;
+
     @OneToMany(mappedBy = "dailyCourse", fetch = FetchType.LAZY)
     private List<DailyCourseFavorite> dailyCourseFavorites;
 
--- src/main/java/com/travelock/server/domain/FullCourse.java
+++ src/main/java/com/travelock/server/domain/FullCourse.java
@@ -17,6 +17,27 @@ public class FullCourse implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long fullCourseId;
 
+    @Column(columnDefinition = "VARCHAR(100) NULL COMMENT '여행 타이틀'")
+    private String title;
+
+    @Column(columnDefinition="INT COMMENT '좋아요 수'")
+    private Integer favoriteCount;
+
+    @Column(columnDefinition = "INT COMMENT '스크랩 수'")
+    private Integer scarpCount;
+
+    @Column(columnDefinition = "VARCHAR(1) COMMENT '활성화 상태'")
+    private String activeStatus;
+
+    // Full Course : Member = N : 1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // Full Course : Daily Course = 1 : N
+    @OneToMany(mappedBy = "fullCourse")
+    private List<DailyCourse> dailyCourses;
+
     @OneToMany(mappedBy = "fullCourse", fetch = FetchType.LAZY)
     private List<FullCourseFavorite> fullCourseFavorites;
 
