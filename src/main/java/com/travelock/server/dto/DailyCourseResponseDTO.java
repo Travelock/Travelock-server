94cab6cd8c5a4496e408ba970903b884955cf132
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-09-29 10:40:30 +0900

    Update DailyCourseResponseDTO.java

--- src/main/java/com/travelock/server/dto/DailyCourseResponseDTO.java
+++ src/main/java/com/travelock/server/dto/DailyCourseResponseDTO.java
@@ -14,6 +14,7 @@ public class DailyCourseResponseDTO {
     private Integer dayNum;         // 일자 정보(N일차)
     private Long fullCourseId;      // 포함되어있는 전체일정 ID
     private Long memberId;          // 생성한 멤버 ID
+    private String memberNickName;  // 생성한 멤버 닉네임 - JK
     private Integer favoriteCount;  // 좋아요 수
     private Integer scarpCount;     // 스크랩 수
     private List<DailyBlockConnectDTO> dailyBlockConnects;
