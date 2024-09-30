af3a41365af226ac40f8c207640d96d7aa8ab01a
Ji Yun Han <jiyunhan@Jiui-MacBookAir.local>
2024-09-30 17:40:37 +0900

    지윤 커밋

--- src/main/java/com/travelock/server/filter/JWTUtil.java
+++ src/main/java/com/travelock/server/filter/JWTUtil.java
@@ -43,6 +43,7 @@ public class JWTUtil {
         Claims claims = Jwts.claims();
         claims.put("username", username);
         claims.put("role", role);
+        claims.put("memberId", memberId); // memberId 추가
 
         return Jwts.builder()
                 .setClaims(claims)
@@ -56,6 +57,7 @@ public class JWTUtil {
 
         Claims claims = Jwts.claims();
         claims.put("username", username);
+        claims.put("memberId", memberId);
 
         return Jwts.builder()
                 .setClaims(claims)
