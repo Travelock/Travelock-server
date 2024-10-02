a802fe6aff74454c33122864149dfae7eb1d0278
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-02 17:45:20 +0900

    Update RedisConfig.java

--- src/main/java/com/travelock/server/config/RedisConfig.java
+++ src/main/java/com/travelock/server/config/RedisConfig.java
@@ -1,78 +1,78 @@
-//package com.travelock.server.config;
-//
-//import com.fasterxml.jackson.annotation.JsonTypeInfo;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-//import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-//import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@Configuration
-//@EnableCaching
-//@AutoConfigureAfter(RedisAutoConfiguration.class)
-//public class RedisConfig {
-//
-//    @Value("${spring.data.redis.host}")
-//    private String host;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int port;
-//
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory(){
-//        return new LettuceConnectionFactory(host, port);
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> jsonRedisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(redisConnectionFactory());
-//
-//        // ObjectMapper 설정: 타입 정보를 포함하여 직렬화/역직렬화 오류 방지
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.activateDefaultTyping(
-//                objectMapper.getPolymorphicTypeValidator(),
-//                ObjectMapper.DefaultTyping.NON_FINAL,
-//                JsonTypeInfo.As.PROPERTY
-//        );
-//
-//        // GenericJackson2JsonRedisSerializer를 사용하여 타입 정보를 포함하여 직렬화
-//        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-//
-//
-//        // 키와 값을 위한 직렬화 설정
-////        객체나 구조화된 데이터(json) 저장시 사용.
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(serializer);
-//        template.setHashKeySerializer(new StringRedisSerializer());
-//        template.setHashValueSerializer(serializer);
-//        template.afterPropertiesSet();
-//
-//        return template;
-//    }
-//
-//    // 두 번째 RedisTemplate: String 직렬화 사용
-//    @Bean
-//    public RedisTemplate<String, String> customStringRedisTemplate() {
-//        RedisTemplate<String, String> template = new RedisTemplate<>();
-//        template.setConnectionFactory(redisConnectionFactory());
-//
-//        // String 직렬화 설정
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new StringRedisSerializer());
-//        template.setHashKeySerializer(new StringRedisSerializer());
-//        template.setHashValueSerializer(new StringRedisSerializer());
-//        template.afterPropertiesSet();
-//
-//        return template;
-//    }
-//
-//
-//}
+package com.travelock.server.config;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisConfig {
+
+   @Value("${spring.data.redis.host}")
+   private String host;
+
+   @Value("${spring.data.redis.port}")
+   private int port;
+
+   @Bean
+   public RedisConnectionFactory redisConnectionFactory(){
+       return new LettuceConnectionFactory(host, port);
+   }
+
+   @Bean
+   public RedisTemplate<String, Object> jsonRedisTemplate() {
+       RedisTemplate<String, Object> template = new RedisTemplate<>();
+       template.setConnectionFactory(redisConnectionFactory());
+
+       // ObjectMapper 설정: 타입 정보를 포함하여 직렬화/역직렬화 오류 방지
+       ObjectMapper objectMapper = new ObjectMapper();
+       objectMapper.activateDefaultTyping(
+               objectMapper.getPolymorphicTypeValidator(),
+               ObjectMapper.DefaultTyping.NON_FINAL,
+               JsonTypeInfo.As.PROPERTY
+       );
+
+       // GenericJackson2JsonRedisSerializer를 사용하여 타입 정보를 포함하여 직렬화
+       GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+
+       // 키와 값을 위한 직렬화 설정
+//        객체나 구조화된 데이터(json) 저장시 사용.
+       template.setKeySerializer(new StringRedisSerializer());
+       template.setValueSerializer(serializer);
+       template.setHashKeySerializer(new StringRedisSerializer());
+       template.setHashValueSerializer(serializer);
+       template.afterPropertiesSet();
+
+       return template;
+   }
+
+   // 두 번째 RedisTemplate: String 직렬화 사용
+   // @Bean
+   // public RedisTemplate<String, String> customStringRedisTemplate() {
+   //     RedisTemplate<String, String> template = new RedisTemplate<>();
+   //     template.setConnectionFactory(redisConnectionFactory());
+
+   //     // String 직렬화 설정
+   //     template.setKeySerializer(new StringRedisSerializer());
+   //     template.setValueSerializer(new StringRedisSerializer());
+   //     template.setHashKeySerializer(new StringRedisSerializer());
+   //     template.setHashValueSerializer(new StringRedisSerializer());
+   //     template.afterPropertiesSet();
+
+   //     return template;
+   // }
+
+
+}
