f0ce306230b9c6edeeab8731bc963079f0c51485
Ji Yun Han <jiyunhan@Jiui-MacBookAir.local>
2024-10-03 11:13:19 +0900

    수정필요

--- src/main/java/com/travelock/server/controller/TokenController.java
+++ src/main/java/com/travelock/server/controller/TokenController.java
@@ -30,14 +30,10 @@ public class TokenController {
         }
 
         String username = jwtUtil.getUsername(refreshToken);
-
-        Number memberId = jwtUtil.getMemberId(refreshToken);
-        String newAccessToken = jwtUtil.createJwt(username, "ROLE_USER", 60*60*60L, (Long) memberId); // 새로운 액세스 토큰 발급
-
+        String newAccessToken = jwtUtil.createJwt(username, "ROLE_USER", 60*60*60L, memberId); // 새로운 액세스 토큰 발급
 
         // 새로운 액세스 토큰을 반환
         return ResponseEntity.ok().header("Authorization", newAccessToken).build();
-        // 수정수정, 필터도 수정수정 -> 이해 안가서 다시 물어보기
         // 헤더에 넣지 말고, 쿠키에 넣어서 사용(httponly) -> 토큰만 리턴하게 하고, 필터에서 쿠키를 만드러 req,res 객체로 전달하기.
     }
 }
