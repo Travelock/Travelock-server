275a8d61df250ebe91cba6904e2d34d8a2d6ed1f
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-08 19:20:01 +0900

    Update DailyCourseResponseDTO.java

--- src/main/java/com/travelock/server/dto/DailyCourseResponseDTO.java
+++ src/main/java/com/travelock/server/dto/DailyCourseResponseDTO.java
@@ -5,6 +5,7 @@ import com.travelock.server.domain.DailyCourse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
