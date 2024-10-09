5550803af120b7c9fa0429da32094c0a09c2a7c1
Kim Jae Keun <85093525+klolarion@users.noreply.github.com>
2024-10-09 21:48:31 +0900

    Update RedisConfig.java

--- src/main/java/com/travelock/server/config/RedisConfig.java
+++ src/main/java/com/travelock/server/config/RedisConfig.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
