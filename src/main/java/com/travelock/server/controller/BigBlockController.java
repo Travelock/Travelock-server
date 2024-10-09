504f5392bb82ff1d43864d8b84c76e58e0ddc43e
AN TAEO <hantive98@gmail.com>
2024-10-09 17:31:20 +0900

    Big Swagger

--- src/main/java/com/travelock/server/controller/BigBlockController.java
+++ src/main/java/com/travelock/server/controller/BigBlockController.java
@@ -3,7 +3,14 @@ package com.travelock.server.controller;
 import com.travelock.server.domain.BigBlock;
 import com.travelock.server.dto.BigBlockResponseDTO;
 import com.travelock.server.dto.StateResponseDTO;
+import com.travelock.server.dto.course.daily.DailyCourseRequestDTO;
 import com.travelock.server.service.BigBlockService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,21 +26,51 @@ import java.util.List;
 public class BigBlockController {
     private final BigBlockService bigBlockService;
 
-    // 전체 시/도 목록 조회
+    @Operation(summary = "도/광역시/특별시 목록 조회",
+            tags = {"빅블록 API - V1"},
+            description = "모든 도/광역시/특별시 목록을 조회.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(mediaType = "application/json"))
+            })
     @GetMapping("/list")
     public ResponseEntity<List<StateResponseDTO>> getAllStates() {
         List<StateResponseDTO> states = bigBlockService.getAllStates();
         return ResponseEntity.ok(states);
     }
 
-    // 특정 시/도의 시/군/구(BigBlock) 목록 조회
+
+    @Operation(summary = "특정 시/도의 시/군/구 목록 조회",
+            tags = {"빅블록 API - V1"},
+            description = "특정 시/도의 시/군/구 목록을 조회.",
+            parameters = {
+                    @Parameter(name = "stateCode", description = "도/광역시/특별시 코드", required = true, in = ParameterIn.PATH)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(mediaType = "application/json"))
+            })
     @GetMapping("/{stateCode}/cities")
     public ResponseEntity<List<BigBlockResponseDTO>> getCitiesByState(@PathVariable String stateCode) {
         List<BigBlockResponseDTO> cities = bigBlockService.getCitiesByState(stateCode);
         return ResponseEntity.ok(cities);
     }
 
-    // 특정 시/도에 속한 시/군/구(BigBlock) 조회
+
+    @Operation(summary = "특정 시/군/구 조회",
+            tags = {"빅블록 API - V1"},
+            description = "특정 시/군/구(BigBlock)를 조회.",
+            parameters = {
+                    @Parameter(name = "stateCode", description = "도/광역시/특별시 코드", required = true, in = ParameterIn.PATH),
+                    @Parameter(name = "cityCode", description = "시/군/구 코드", required = true, in = ParameterIn.PATH)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "404", description = "해당 시/군/구를 찾을 수 없음", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(mediaType = "application/json"))
+            })
     @GetMapping("/{stateCode}/{cityCode}")
     public ResponseEntity<BigBlockResponseDTO> getBigBlock(@PathVariable String stateCode, @PathVariable String cityCode) {
         BigBlockResponseDTO responseDTO = bigBlockService.getBigBlock(stateCode, cityCode);
--- src/main/java/com/travelock/server/repository/BigBlockRepository.java
+++ src/main/java/com/travelock/server/repository/BigBlockRepository.java
@@ -1,10 +1,8 @@
 package com.travelock.server.repository;
 
 import com.travelock.server.domain.BigBlock;
-import com.travelock.server.domain.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface BigBlockRepository extends JpaRepository<BigBlock, Long> {
 }
